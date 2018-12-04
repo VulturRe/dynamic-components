537910d5078639c8b0835f67ca06f9d964e0e9df
Ilshat Galimov <einnks@gmail.com>
2018-12-04 21:29:16 +0300

    Base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentHostDirective } from './_directives/component-host.directive';
+import { ContentWrapperComponent } from './components/content-wrapper/content-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentHostDirective,
+    ContentWrapperComponent
   ],
   imports: [
     BrowserModule,
